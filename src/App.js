dcd2d62620354da8e98076bb20f112d0c8b49b6e
MeryAmun <chebesiawah@gmail.com>
2022-03-15 13:37:02 +0100

    basic redux increment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from 'react-redux';
+import './App.css';
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+  dispatch({type:'INC'})
+  }
+
+  const decrement = () => {
+    dispatch()
+  }
+  return (
+    <div className="App">
+   <h1>Counter App</h1>
+  <h2>{counter}</h2>
+  <button onClick={increment}>Increment</button>
+  <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
